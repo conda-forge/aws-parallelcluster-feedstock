0acc494e5b3653455fe5cb4388e0d11c0653b8a5
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-06-10 12:28:03 -0400

    added a warning message that 2.3.2a1 is an alpha release.  added two aws-parallelcluster developers as maintainers.

---
+++ recipe/post-link.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cat << EOF
+  ------------------------------------------------------------------------------
+
+  Please note:  AWS ParallelCluster 2.3.2a1 is an alpha version.  For production
+  use consider switching to 2.3.1 or wait for the new stable release coming soon.
+
+  ------------------------------------------------------------------------------ 
+EOF
